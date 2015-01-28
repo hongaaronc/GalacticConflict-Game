a1264c3134d3cd1794aaa4aa3c0ba65e02c4a194
Aaron Hong <aaronhon@usc.edu>
2015-01-28 15:21:10 -0800

    Added bullets and nebula background

---
+++ Assets/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+    public float fireSpeed;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().AddRelativeForce(fireSpeed * Vector3.forward);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/WeaponSystem.cs
+++ Assets/WeaponSystem.cs
@@ -5,30 +5,46 @@ public class WeaponSystem : GenericSystem {
     public GameObject myWeapon;
     public int[] fireTimes;
     public int coolDown;
+    public bool triggerOnce;        //If true, key only needs to be tapped once for the whole system to trigger. If false, key needs to be held down to continue 
+
+    private bool myKeyDown = false;
     private int timer = 0;
+
+    private NetworkManager myNetworkManager;
 	// Use this for initialization
 	void Start () {
         timer = fireTimes[fireTimes.Length - 1] + coolDown + 1;
+        myNetworkManager = Camera.main.GetComponent<NetworkManager>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (timer <= fireTimes[fireTimes.Length-1]+coolDown)
-        {
-            foreach (int time in fireTimes)
+            if (timer <= fireTimes[fireTimes.Length - 1] + coolDown)
             {
-                if (time == timer)
+                bool fireReady = false;
+                foreach (int time in fireTimes)
                 {
-                    Instantiate(myWeapon, transform.position, transform.rotation);
-                }
+                    if (time == timer)
+                    {
+                        if (triggerOnce || myKeyDown)
+                        {
+                            if (myNetworkManager.multiplayerEnabled)
+                                Network.Instantiate(myWeapon, transform.position, transform.rotation, 0);
+                            else
+                                Instantiate(myWeapon, transform.position, transform.rotation);
+                        }
+                        fireReady = true;
+                    }
 
+                }
+                timer++;
             }
-            timer++;
-        }
+        myKeyDown = false;
 	}
 
     public override void Activate()
     {
+        myKeyDown = true;
         if (timer > fireTimes[fireTimes.Length - 1] + coolDown)
             timer = 0;
     }
