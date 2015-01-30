eb27c35b0e5501b5b8925c4ffd411e19ff39fa70
Aaron Hong <aaronhon@usc.edu>
2015-01-30 01:23:12 -0800

    Changed Star Spawning/Respawning
    Now works for orthographic perspective

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -3,16 +3,34 @@ using System.Collections;
 
 public class Star : MonoBehaviour {
 	public float range = 100f;
+    public float parallaxMult = 1f;
 	// Use this for initialization
 	void Start () {
-	
+        GetComponent<LensFlare>().brightness = Random.Range(0.5f, 5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if((transform.position-Camera.main.transform.position).magnitude > range) {
-			transform.position = Camera.main.transform.position + Random.onUnitSphere * range;
-		}
+        if (transform.position.x > Camera.main.transform.position.x + Camera.main.orthographicSize * Screen.width / Screen.height)
+        {
+            transform.position -= 2f * new Vector3(Camera.main.orthographicSize * Screen.width / Screen.height,0f,0f);
+            GetComponent<LensFlare>().brightness = Random.Range(0.5f, 5f);
+        }
+        else if (transform.position.x < Camera.main.transform.position.x - Camera.main.orthographicSize * Screen.width / Screen.height)
+        {
+            transform.position += 2f * new Vector3(Camera.main.orthographicSize * Screen.width / Screen.height, 0f, 0f);
+            GetComponent<LensFlare>().brightness = Random.Range(0.5f, 5f);
+        }
+        if (transform.position.z > Camera.main.transform.position.z + Camera.main.orthographicSize)
+        {
+            transform.position -= 2f * new Vector3(0f, 0f, Camera.main.orthographicSize);
+            GetComponent<LensFlare>().brightness = Random.Range(0.5f, 5f);
+        }
+        else if (transform.position.z < Camera.main.transform.position.z - Camera.main.orthographicSize)
+        {
+            transform.position += 2f * new Vector3(0f, 0f, Camera.main.orthographicSize);
+            GetComponent<LensFlare>().brightness = Random.Range(0.5f, 5f);
+        }
 	}
 }
     
--- Assets/Scripts/StarSpawner.cs
+++ Assets/Scripts/StarSpawner.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class StarSpawner : MonoBehaviour {
 	public int numStars = 100;
-	public float range = 100f;
 	public GameObject myStar;
 	// Use this for initialization
 	void Start () {
 		for (int i=0; i<numStars; i++) {
-            GameObject newStar = (GameObject)Instantiate(myStar, transform.position + Random.insideUnitSphere * range, Quaternion.identity);
+            Vector3 newPosition = new Vector3(Camera.main.transform.position.x + Random.Range(-Camera.main.orthographicSize * Screen.width / Screen.height, Camera.main.orthographicSize * Screen.width / Screen.height), Camera.main.transform.position.y-50f, Camera.main.transform.position.z + Random.Range(-Camera.main.orthographicSize, Camera.main.orthographicSize));
+            GameObject newStar = (GameObject)Instantiate(myStar, newPosition, Quaternion.identity);
             newStar.transform.parent = transform;
 		}
 	}
