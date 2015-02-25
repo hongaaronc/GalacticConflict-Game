915bca1ac29ed1556299819a573c7bd88d85c2de
Aaron Hong <aaronhon@usc.edu>
2015-02-24 19:19:16 -0800

    Cursor script

--- Assets/MouseInput.cs
+++ Assets/MouseInput.cs
@@ -3,21 +3,40 @@ using System.Collections;
 
 public class MouseInput : MonoBehaviour {
 
+    public Sprite cursorIdle;
+    public Sprite cursorHover;
+
+    Vector3 cursorPosition;
+    Vector3 mousePosition = Vector3.zero;
+    Vector3 lastMousePosition = Vector3.zero;
+
 	// Use this for initialization
 	void Start () {
-	
+        //Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        //cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //cursorPosition.y = Camera.main.transform.position.y;
+        lastMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lastMousePosition.y = Camera.main.transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 mousePosition;
+        //Cursor.SetCursor(cursorIdle, Vector2.zero, CursorMode.Auto);
+        GetComponent<SpriteRenderer>().sprite = cursorIdle;
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.y = Camera.main.transform.position.y;
-        Debug.DrawRay(mousePosition, -Vector3.up, Color.green);
+        transform.position += mousePosition - lastMousePosition;
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 10f);
+        Debug.DrawLine(transform.position, transform.position - 100f * Vector3.up, Color.green);
         RaycastHit hit;
-        if (Physics.Raycast(mousePosition, -Vector3.up, out hit, 100.0F))
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, 100.0F))
         {
-            Debug.DrawLine(mousePosition, hit.point, Color.red);
-        }       
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+            //Cursor.SetCursor(cursorHover, Vector2.zero, CursorMode.Auto);
+            GetComponent<SpriteRenderer>().sprite = cursorHover;
+            print("hit");
+        }
+        lastMousePosition = mousePosition;
 	}
 }
