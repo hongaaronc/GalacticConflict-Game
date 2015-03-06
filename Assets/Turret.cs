1a84273afdef6f6ac5170c3928b9074df1e00627
Aaron Hong <aaronhon@usc.edu>
2015-03-05 19:07:43 -0800

    Added turret constraints

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class Turret : MonoBehaviour {
     public Vector3 targetVector;
-    public float turnRate = 2;
+    public float turnRate = 2f;
+    public float turnRange = 360f;
+
+    private float parentedAngleOffset = 0f;
 
 	private NetworkView myNetworkView;
     private NetworkManager myNetworkManager;
 	// Use this for initialization
 	void Start () {
         targetVector = Camera.main.GetComponent<CameraFollow>().mousePosition;
+
+        parentedAngleOffset += GetComponentInParent<Transform>().localEulerAngles.y;
 
         myNetworkView = GetComponent<NetworkView>();
         myNetworkManager = Camera.main.GetComponent<NetworkManager>();
@@ -20,9 +25,15 @@ public class Turret : MonoBehaviour {
         if (!myNetworkManager.multiplayerEnabled || myNetworkView.isMine)
         {
             targetVector = Camera.main.GetComponent<CameraFollow>().mousePosition;
-            float targetAngle = Mathf.Atan2(transform.position.z - targetVector.z, targetVector.x - transform.position.x) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(transform.position.z - targetVector.z, targetVector.x - transform.position.x) * Mathf.Rad2Deg + parentedAngleOffset;
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnRate), transform.eulerAngles.z);
-            transform.localEulerAngles = new Vector3(0f, transform.localEulerAngles.y, 0f);
+            float newAngle = transform.localEulerAngles.y;
+            if (newAngle > 180f)
+                newAngle -= 360f;
+            if (newAngle < -180f)
+                newAngle += 360f;
+            newAngle = Mathf.Clamp(newAngle, -turnRange, turnRange);
+            transform.localEulerAngles = new Vector3(0f, newAngle, 0f);
         }
 	}
 }
