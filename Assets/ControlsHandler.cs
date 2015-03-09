8551d116550cf75eb5c66b1b01b82a5cde7f2308
hongaaronc <hong.aaron.c@gmail.com>
2015-03-08 19:33:55 -0700

    Input stuff

---
+++ Assets/ControlsHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlsHandler : MonoBehaviour {
+    public Transform myPlayer;
+    public Transform GUIcursor;
+    public Vector3 mousePosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mousePosition = Camera.main.ScreenToWorldPoint(GUIcursor.position);
+        mousePosition.y = 0f;
+	}
+}
--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -9,6 +9,9 @@ public class CameraFollow : MonoBehaviour {
     public float easing = 2f;
     public float leadMultiplier = 5f;
     public float maxLead = 30f;
+    public bool freeCam = false;
+    public float scrollMargin = 5f;
+    public float scrollSpeed = 2f;
 	// Use this for initialization
 	void Start () {
         try
@@ -21,14 +24,31 @@ public class CameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        try
+        if (Input.GetMouseButton(1))
+            freeCam = true;
+        else
+            freeCam = false;
+        if (freeCam)
         {
-            Vector3 leadValue = Vector3.ClampMagnitude(leadMultiplier * myTargets[0].velocity, maxLead);
-            transform.position += ((new Vector3(myTargets[0].transform.position.x, distance, myTargets[0].transform.position.z) + leadValue) - transform.position) / easing;
+            if (GUIcursor.position.x < scrollMargin)
+                transform.Translate(scrollSpeed * Vector3.left);
+            if (GUIcursor.position.x > Screen.width - scrollMargin)
+                transform.Translate(scrollSpeed * Vector3.right);
+            if (GUIcursor.position.y < scrollMargin)
+                transform.Translate(scrollSpeed * Vector3.down);
+            if (GUIcursor.position.y > Screen.height - scrollMargin)
+                transform.Translate(scrollSpeed * Vector3.up);
         }
-        catch { }
-		transform.eulerAngles = new Vector3 (90f, 0f, 0f);
-
+        else
+        {
+            try
+            {
+                Vector3 leadValue = Vector3.ClampMagnitude(leadMultiplier * myTargets[0].velocity, maxLead);
+                transform.position += ((new Vector3(myTargets[0].transform.position.x, distance, myTargets[0].transform.position.z) + leadValue) - transform.position) / easing;
+            }
+            catch { }
+            transform.eulerAngles = new Vector3(90f, 0f, 0f);
+        }
         mousePosition = Camera.main.ScreenToWorldPoint(GUIcursor.position);
         mousePosition.y = 0f;
 	}
