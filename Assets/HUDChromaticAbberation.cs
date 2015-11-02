f929bb8089c73bd9705c1b3379f59c0abeb5b704
Aaron Hong <hong.aaron.c@gmail.com>
2015-11-02 04:22:42 -0800

    Fine Tweaks to HUD

--- Assets/HUDChromaticAbberation.cs
+++ Assets/HUDChromaticAbberation.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityStandardAssets.ImageEffects;
 
 public class HUDChromaticAbberation : MonoBehaviour {
-    public CustomVignetteAndChromaticAbberation effect;
+    public CustomVignetteAndChromaticAbberation vignetteAndChrom;
     public float baseAberation = 2f;
     public float testAberation;
     public bool test = false;
@@ -19,25 +19,30 @@ public class HUDChromaticAbberation : MonoBehaviour {
     {
         if (test)
         {
-            effect.chromaticAberration = baseAberation;
+            vignetteAndChrom.chromaticAberration = baseAberation;
             test = false;
         }
     }
 
 	// Use this for initialization
 	void Start () {
-        effect.chromaticAberration = baseAberation;
+        vignetteAndChrom.chromaticAberration = baseAberation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.F1)) {
-            effect.chromaticAberration = testAberation;
+            distort(testAberation);
         }
         changeVelocity *= 1f - dampening * Time.deltaTime;
-        changeVelocity += springiness * (baseAberation - effect.chromaticAberration) * Time.deltaTime;
+        changeVelocity += springiness * (baseAberation - vignetteAndChrom.chromaticAberration) * Time.deltaTime;
         changeVelocity += Random.Range(-jitterAberation, jitterAberation) * Time.deltaTime;
-        effect.chromaticAberration += changeVelocity;
-        effect.intensity = baseVignette + Random.Range(-jitterVignette, jitterVignette);
+        vignetteAndChrom.chromaticAberration += changeVelocity;
+        vignetteAndChrom.intensity = baseVignette + Random.Range(-jitterVignette, jitterVignette);
 	}
+
+    public void distort(float value)
+    {
+        vignetteAndChrom.chromaticAberration += value;
+    }
 }
