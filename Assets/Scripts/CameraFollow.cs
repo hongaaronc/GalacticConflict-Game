01fa90b2df66c3c76f0b033b1054121963a5a33f
Aaron Hong <hong.aaron.c@gmail.com>
2015-11-02 05:31:35 -0800

    Temporary chromatic abberation effect plays on touching walls

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -9,11 +9,12 @@ public class CameraFollow : MonoBehaviour {
     public float easing = 2f;
     public float leadMultiplier = 5f;
     public float maxLead = 30f;
-    public bool freeCam = false;
-    public float scrollMargin = 5f;
-    public float scrollSpeed = 2f;
-	// Use this for initialization
+    Vector3 velocity;
+    public float dampTime = 1.0f;
+
+    // Use this for initialization
 	void Start () {
+        velocity = Vector3.zero;
         try
         {
             Vector3 leadValue = Vector3.ClampMagnitude(leadMultiplier * myTargets[0].velocity, maxLead);
@@ -28,31 +29,14 @@ public class CameraFollow : MonoBehaviour {
         {
             Camera.main.GetComponent<NetworkManager>().spawnShip();
         }
-        if (Input.GetMouseButton(1))
-            freeCam = true;
-        else
-            freeCam = false;
-        if (freeCam)
+        try
         {
-            if (GUIcursor.position.x < scrollMargin)
-                transform.Translate(scrollSpeed * Vector3.left);
-            if (GUIcursor.position.x > Screen.width - scrollMargin)
-                transform.Translate(scrollSpeed * Vector3.right);
-            if (GUIcursor.position.y < scrollMargin)
-                transform.Translate(scrollSpeed * Vector3.down);
-            if (GUIcursor.position.y > Screen.height - scrollMargin)
-                transform.Translate(scrollSpeed * Vector3.up);
+            Vector3 leadValue = Vector3.ClampMagnitude(leadMultiplier * myTargets[0].velocity, maxLead);
+            //transform.position += ((new Vector3(myTargets[0].transform.position.x, distance, myTargets[0].transform.position.z) + leadValue) - transform.position) / easing;
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(myTargets[0].transform.position.x, distance, myTargets[0].transform.position.z) + leadValue, ref velocity, dampTime);
         }
-        else
-        {
-            try
-            {
-                Vector3 leadValue = Vector3.ClampMagnitude(leadMultiplier * myTargets[0].velocity, maxLead);
-                transform.position += ((new Vector3(myTargets[0].transform.position.x, distance, myTargets[0].transform.position.z) + leadValue) - transform.position) / easing;
-            }
-            catch { }
-            transform.eulerAngles = new Vector3(90f, 0f, 0f);
-        }
+        catch { }
+        transform.eulerAngles = new Vector3(90f, 0f, 0f);
         mousePosition = Camera.main.ScreenToWorldPoint(GUIcursor.position);
         mousePosition.y = 0f;
 	}
--- Assets/Scripts/GUIHudArrow.cs
+++ Assets/Scripts/GUIHudArrow.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 
 public class GUIHudArrow : MonoBehaviour {
+    public GameObject playerShip;
+    private Rigidbody playerRigidbody;
     public GameObject targetShip;
+    private Rigidbody targetRigidbody;
+    public GameObject model;
+    private Rigidbody modelRigidbody;
 	// Use this for initialization
 	void Start () {
-	
+        playerRigidbody = playerShip.GetComponent<Rigidbody>();
+        targetRigidbody = targetShip.GetComponent<Rigidbody>();
+        modelRigidbody = model.GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        Vector3 relativeVelocity = targetRigidbody.velocity - playerRigidbody.velocity;
+        modelRigidbody.angularVelocity = new Vector3(relativeVelocity.z, targetRigidbody.angularVelocity.y, -relativeVelocity.x);
 	}
 }
