ccec3471310e79414bd9cb9a7bf498de05c4d9d2
Aaron Hong <hong.aaron.c@gmail.com>
2015-11-02 05:51:44 -0800

    HUD Coordinates, Fine tweaks.

---
+++ Assets/HUDCoordinates3D.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDCoordinates3D : MonoBehaviour
+{
+    public TextMesh text;
+    public bool updating = true;
+    public float refreshRate = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(updateText());
+    }
+
+
+    IEnumerator updateText()
+    {
+        text.text = "X:" + ((int)Camera.main.transform.position.x).ToString() + "  Y:" + ((int)Camera.main.transform.position.z).ToString();
+        yield return new WaitForSeconds(refreshRate);
+        if (updating)
+            StartCoroutine(updateText());
+    }
+}
