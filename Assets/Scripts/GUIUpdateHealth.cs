8562355eaab274dfe96782c1bf6bf069c3170726
Aaron Hong <hong.aaron.c@gmail.com>
2015-11-01 23:10:58 -0800

    Fixed some stuff.

--- Assets/Scripts/GUIUpdateHealth.cs
+++ Assets/Scripts/GUIUpdateHealth.cs
@@ -7,11 +7,17 @@ public class GUIUpdateHealth : MonoBehaviour
     public Text text;
     public bool updating = true;
     public float refreshRate = 0.1f;
+    public Slider hullSlider;
+    public Slider shieldSlider;
 
     // Use this for initialization
     void Start()
     {
         StartCoroutine(updateText());
+        shieldSlider.maxValue = 100;
+        hullSlider.maxValue = 100;
+        shieldSlider.minValue = 0;
+        hullSlider.minValue = 0;
     }
 
 
@@ -19,7 +25,11 @@ public class GUIUpdateHealth : MonoBehaviour
     {
         if (Camera.main.GetComponent<CameraFollow>().myTargets[0] != null)
             if (Camera.main.GetComponent<CameraFollow>().myTargets[0].GetComponent<Health>() != null)
+            {
                 text.text = "HULL:" + ((int)Camera.main.GetComponent<CameraFollow>().myTargets[0].GetComponent<Health>().myHealth) + " | SHIELD:" + ((int)Camera.main.GetComponent<CameraFollow>().myTargets[0].GetComponent<Health>().myShield);
+                hullSlider.value = (int)Camera.main.GetComponent<CameraFollow>().myTargets[0].GetComponent<Health>().myHealth;
+                shieldSlider.value = (int)Camera.main.GetComponent<CameraFollow>().myTargets[0].GetComponent<Health>().myShield;
+            }
         yield return new WaitForSeconds(refreshRate);
         if (updating)
             StartCoroutine(updateText());
